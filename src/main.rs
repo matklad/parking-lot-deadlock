12945a9909f2969001449e5034abb686a3c8c2c1
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-12-27 15:59:48 +0300

    deadlock variation

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
 extern crate parking_lot;
 
 use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::SeqCst;
 
-use parking_lot::RwLock;
+use parking_lot::{RwLock, RwLockUpgradableReadGuard};
 
 fn main() {
-    let data = Arc::new(RwLock::new(()));
-    let flag = Arc::new(AtomicBool::new(false));
-    let N = 100_000;
+    let data = Arc::new(RwLock::new(0u64));
+    const N: usize = 1_000_000;
     let mut upgraders = Vec::new();
-    for _ in 0..10 {
+    for _ in 0..8 {
         let t = {
             let data = Arc::clone(&data);
-            let flag = flag.clone();
             std::thread::spawn(move || {
-                for _ in 0..N {
+                for i in 0..N {
                     let guard = data.upgradable_read();
-                    while flag.load(SeqCst) {}
-                    drop(guard);
+                    if i % 2 == 0 {
+                        let mut guard = RwLockUpgradableReadGuard::upgrade(guard);
+                        *guard += 1;
+                    } else {
+                        drop(guard);
+                        let mut guard = data.write();
+                        *guard += 1;
+                    }
+                    if i % 10 == 9 {
+                        assert!(*data.read() > 1)
+                    }
                 }
             })
         };
         upgraders.push(t);
     }
 
-    for i in 0..N {
-        eprintln!("i = {:?}", i);
-        flag.store(true, SeqCst);
-        let read = data.read();
-        flag.store(false, SeqCst);
-        drop(read);
-    }
-    println!("reader done");
+    eprintln!("waiting for threads to finish...");
     upgraders.into_iter().for_each(|it| drop(it.join()));
-    println!("upgrader locked");
+    eprintln!("done, final value: {}", *data.read());
 }
